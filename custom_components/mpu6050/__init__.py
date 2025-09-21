49db54640cde610cb5641c51d01c5b7bab2864dd
Markus Hütter <huetter@outlook.com>
2025-09-21 20:51:07 +0200

    Ensure runtime data is stopped during config entry unload

--- custom_components/mpu6050/__init__.py
+++ custom_components/mpu6050/__init__.py
@@ -22,6 +22,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
+    entry.runtime_data.stop()
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data["mpu6050"].pop(entry.entry_id, None)
